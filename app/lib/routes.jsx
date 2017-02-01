36f62b72bc6b948d7da1ed47f353f46be6133de7
francescobarbera <francesco.barbera@mondora.com>
2017-02-01 22:31:41 +0100

    [fix] fixes a lint issue

--- app/lib/routes.jsx
+++ app/lib/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {browserHistory ,Router, Route} from 'react-router';
+import {browserHistory, Router, Route} from 'react-router';
 
 import Root from 'containers/Root';
 
