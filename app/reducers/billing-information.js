624cda15507a1a69bf0a031c29b4bad723b1439f
Davide Morri <davide.morri@mondora.com>
2017-02-13 16:04:18 +0900

    [feat] add billing information component using react-redux-form

---
+++ app/reducers/billing-information.js
@@ -0,0 +1,16 @@
+const billingInformation = {
+    givenName: '',
+    familyName: '',
+    company: '',
+    pIVA: '',
+    email: '',
+    phoneNumber: '',
+    address: '',
+    houseNumber: '',
+    CAP: '',
+    city: '',
+    province: '',
+    country: ''
+};
+
+export default billingInformation;
