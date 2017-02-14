66523c8b11e54424f3b58a42e191743ea044bfbd
francesca <francesca.giudici@mondora.com>
2017-02-14 17:18:48 +0100

    Add component with icon, title and description

---
+++ app/components/FeaturesIcons.jsx
@@ -0,0 +1,45 @@
+import React, {Component, PropTypes} from 'react';
+import Radium from 'radium';
+
+import Icon from 'components/Icon';
+import * as colors from 'lib/colors';
+
+class FeaturesIcons extends Component {
+    static propTypes = {
+        icon: PropTypes.string.isRequired,
+        iconStyle: PropTypes.object,
+        text: PropTypes.string,
+        textColor: PropTypes.string,
+        title: PropTypes.string.isRequired,
+        titleColor: PropTypes.string
+    };
+
+    static defaultProps = {
+        icon: 'responsive',
+        iconStyle: {},
+        text: '',
+        textColor: colors.darkGrey,
+        title: '',
+        titleColor: colors.darkGrey,
+    };
+
+    render () {
+        return (
+            <div>
+                <Icon
+                    iconName={this.props.icon}
+                    iconSize={80}
+                    iconStyle={this.props.iconStyle}
+                />
+                <h3 style={{color: this.props.titleColor, fontWeight: '800'}}>
+                    {this.props.title}
+                </h3>
+                <p style={{color: this.props.textColor}}>
+                    {this.props.text}
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Radium(FeaturesIcons);
