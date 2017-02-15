c5e9dbb4f029401a480bf7add623d234b9c34bf8
Jacopo Gosatti <jgosatti@mondora.com>
2017-02-15 18:18:12 +0100

    add style to loginForm

--- app/components/LoginModal.jsx
+++ app/components/LoginModal.jsx
@@ -5,8 +5,17 @@ import {Link} from 'react-router';
 
 import Button from 'components/CustomButton';
 import FormInput from 'components/FormInput';
+import HorizontalLine from 'components/HorizontalLine';
+import {darkGrey, primaryColor} from 'lib/colors';
+
 
 import {genericRequiredValidator, requiredEmailValidator} from 'lib/form-utils';
+
+// const styles = {
+//     loginModalContainer: {
+//         textAlign: 'center'
+//     }
+// };
 
 export default class LoginModal extends Component {
     static propTypes = {
@@ -31,55 +40,63 @@ export default class LoginModal extends Component {
                 model={'user.login'}
                 onSubmit={this.login.bind(this)}
             >
-                <FormInput
-                    field={this.props.form.email}
-                    inputType='email'
-                    model='user.login.email'
-                    placeholder='Email'
-                    validator={requiredEmailValidator}
-                />
+                <div style={{maxWidth: 400, margin: '0 auto'}}>
+                    <FormInput
+                        field={this.props.form.email}
+                        inputType='email'
+                        model='user.login.email'
+                        placeholder='Email'
+                        validator={requiredEmailValidator}
+                    />
+                    <br />
+                    <FormInput
+                        field={this.props.form.password}
+                        inputType='password'
+                        model='user.login.password'
+                        placeholder='Password'
+                        validator={genericRequiredValidator}
+                    />
 
-                <FormInput
-                    field={this.props.form.password}
-                    inputType='password'
-                    model='user.login.password'
-                    placeholder='Password'
-                    validator={genericRequiredValidator}
-                />
-    
-                {this.props.loginState.errorMessage}
-    
-                <Button
-                    backgroundColor={'#708090'}
-                    text={'LOGIN >'}
-                    type='submit'
-                />
+                    {this.props.loginState.errorMessage}
+                </div>
+
+                <div style={{textAlign: 'center', width: '100%', marginTop: 20}}>
+                    <Button
+                        backgroundColor={darkGrey}
+                        text={'LOGIN >'}
+                        type='submit'
+                    />
+                </div>
+                <b><p style={{textAlign: 'center', marginTop: 20, color: primaryColor, fontSize: 20}}>
+                    {'INIZIA ORA!'}
+                </p></b>
             </Form>
         );
     }
 
     render () {
         return (
-            <Modal show={this.props.show} onHide={this.closeModal.bind(this)}>
-                <Modal.Header closeButton={true}>
-                    <Modal.Title>
-                        {'LOGIN'}
+            <div>
+                <Modal show={this.props.show} onHide={this.closeModal.bind(this)}>
+                    <Modal.Header closeButton={true} style={{border: 0}}>
+                        <Modal.Title style={{textAlign: 'center'}}>
+                            <b style={{fontSize: 25}}>{'LOGIN'}</b>
+                            <br />
+                            <b>{'accedi al tuo account'}</b>
+                            <HorizontalLine color={primaryColor} width={60} />
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {this.renderLoginForm()}
+                    </Modal.Body>
+                    <Modal.Footer style={{textAlign: 'center'}}>
                         <p>
-                            {'accedi al tuo account'}
+                            {'Non hai un account? '}
+                            <Link to='signup' onClick={this.closeModal.bind(this)}>{'Registrati ora'}</Link>
                         </p>
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    {this.renderLoginForm()}
-                </Modal.Body>
-                <Modal.Footer>
-                    {'INIZIA ORA!'}
-                    <p>
-                        {'Non hai un account?'}
-                        <Link to='signup' onClick={this.closeModal.bind(this)}>{'Registrati ora'}</Link>
-                    </p>
-                </Modal.Footer>
-            </Modal>
+                    </Modal.Footer>
+                </Modal>
+            </div>
         );
     }
 }
