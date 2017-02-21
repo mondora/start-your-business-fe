3a693895c5344e8f5cd2ae58b3335481b418bcc2
francesca <francesca.giudici@mondora.com>
2017-02-21 17:28:16 +0100

    Add style to component

--- app/components/UserMenu.jsx
+++ app/components/UserMenu.jsx
@@ -1,7 +1,17 @@
+import Radium from 'radium';
 import React, {Component, PropTypes} from 'react';
 import {DropdownButton, MenuItem} from 'react-bootstrap';
 
-export default class UserMenu extends Component {
+import * as colors from 'lib/colors';
+
+const styles = {
+    blockWrp: {
+        backgroundColor: colors.primaryColor,
+        borderRadius: '30px'
+    }
+};
+
+class UserMenu extends Component {
     static propTypes = {
         logout: PropTypes.func.isRequired,
         user: PropTypes.object.isRequired
@@ -9,18 +19,45 @@ export default class UserMenu extends Component {
 
     render () {
         return (
-            <DropdownButton
-                bsStyle='success'
-                title={`Ciao ${this.props.user.username}`}
-            >
-                <MenuItem eventKey='1'>
-                    {'Il mio account'}
-                </MenuItem>
-                <MenuItem divider={true} />
-                <MenuItem eventKey='2'>
-                    {'Logout'}
-                </MenuItem>
-            </DropdownButton>
+            <div className='dropdown-account'>
+                <DropdownButton
+                    id='dropdown-account'
+                    title={`Ciao ${this.props.user.username}`}
+                    style={styles.buttonWrp}
+                >
+                    <Radium.Style
+                        rules={{
+                            '.btn-default': {
+                                backgroundColor: `${colors.primaryColor} !important`,
+                                borderRadius: '30px !important',
+                                color: `${colors.white} !important`,
+                                paddingLeft: '20px !important',
+                                paddingRight: '20px !important',
+                                height: '42px',
+                                borderColor: `${colors.transparent} !important`
+                            },
+                            '.dropdown-menu': {
+                                width: '88%',
+                                margin: '2px 20px !important'
+                            },
+                            '.dropdown-menu>li>a:focus, .dropdown-menu>li>a:hover': {
+                                backgroundColor: `${colors.transparent} !important`,
+                                color: `${colors.primaryColor} !important`
+                            }
+                        }}
+                        scopeSelector='.dropdown-account'
+                    />
+                    <MenuItem eventKey='1'>
+                        {'Il mio account'}
+                    </MenuItem>
+                    <MenuItem divider={true} />
+                    <MenuItem eventKey='2'>
+                        {'Logout'}
+                    </MenuItem>
+                </DropdownButton>
+            </div>
         );
     }
 }
+
+export default Radium(UserMenu);
